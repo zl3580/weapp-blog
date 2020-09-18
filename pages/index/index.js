d5fe1467d61070068095103e9f4a00279e65915f
zl3580 <35804583@163.com>
2020-09-18 17:24:28 +0800

    commit  首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,35 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    left: '',
+    bottom: 0
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../home/home'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  onShow: function () {
+    let _this = this;
+    wx.startAccelerometer({
+      success: (res) => {
+        console.log(res)
+        wx.onAccelerometerChange(res => {
+          console.log('x', res.x)
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
+    // wx.onAccelerometerChange(function(res) {
+    //   console.log('x',res.x)
+    //   console.log('y',res.y)
+    //   //y <0  bottom +
+    //   let startX=0;
+    //   let startY=-1;
+    //   _this.setData({
+    //     bottom:_this.data.bottom+100
+    //   })
+    // })
+
   }
 })
