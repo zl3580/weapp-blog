cdfc52e4d206dbc6ade6549891abb0e6042984ae
zl3580 <35804583@163.com>
2020-09-25 17:53:31 +0800

    commit  :首页canvas  动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,36 +5,93 @@ const app = getApp()
 
 Page({
   data: {
-    isCancelAnimal: false,
     lists: [{
         color: '#426666',
         font: '写',
         fontsize: '30px sans-serif',
-        radius: 50,
-        translateX: 200,
-        count: 1,
-        position: {
+        radius: 20,
+        defaultPosition: {
           x: 100,
-          y: 150,
-          vx: 2,
-          vy: 15
+          y: 0,
+          vx: 6,
+          vy: 6
         }
       },
       {
         color: '#a98175',
         font: '代',
-        fontsize: '30px sans-serif',
+        fontsize: '25px sans-serif',
         radius: 25,
-        count: 1,
-        translateX: 280,
-        position: {
-          x: 200,
-          y: 150,
-          vx: 2,
-          vy: 15
+        defaultPosition: {
+          x: 150,
+          y: 0,
+          vx: 6,
+          vy: 6
         }
-      }
-    ]
+      },
+      {
+        color: '#75878a',
+        font: '码',
+        fontsize: '10px sans-serif',
+        radius: 30,
+        defaultPosition: {
+          x: 50,
+          y: 0,
+          vx: 4,
+          vy: 4
+        }
+      },
+      {
+        color: '#a78e44',
+        font: '的',
+        fontsize: '30px sans-serif',
+        radius: 26,
+        defaultPosition: {
+          x: 250,
+          y: 0,
+          vx: 4,
+          vy: 4
+        }
+      },
+      {
+        color: '#493131',
+        font: '小',
+        fontsize: '10px sans-serif',
+        radius: 20,
+        defaultPosition: {
+          x: 350,
+          y: 0,
+          vx: 2,
+          vy: 2
+        }
+      },
+      {
+        color: '#6b6882',
+        font: '胖',
+        fontsize: '30px sans-serif',
+        radius: 30,
+        defaultPosition: {
+          x: 350,
+          y: 0,
+          vx: 2,
+          vy: 2
+        }
+      },
+      {
+        color: '#808080',
+        font: '墩',
+        fontsize: '25px sans-serif',
+        radius: 25,
+        defaultPosition: {
+          x: 380,
+          y: 0,
+          vx: 1,
+          vy: 1
+        }
+      },
+    ],
+    height:''
+    
   },
   //事件处理函数
   bindViewTap: function () {
@@ -46,6 +103,9 @@ Page({
     //可使用窗口宽度、高度，单位px
     this.systemWidth = wx.getSystemInfoSync().windowWidth
     this.systemHeight = wx.getSystemInfoSync().windowHeight
+    this.setData({
+      height:this.systemHeight
+    })
     // 通过 SelectorQuery 获取 Canvas 节点
     wx.createSelectorQuery()
       .select('#canvas')
@@ -62,90 +122,157 @@ Page({
     const height = res[0].height
     const canvas = res[0].node
     const ctx = canvas.getContext('2d')
-    console.log('ctx', ctx.font)
     const dpr = wx.getSystemInfoSync().pixelRatio
     canvas.width = width * dpr
     canvas.height = height * dpr
     ctx.scale(dpr, dpr)
-    const renderLoop = () => {
-      this.render(canvas, ctx)
-      canvas.requestAnimationFrame(renderLoop)
+    let requestId;
+    const renderLoop = (gravityX, gravityY ) => {
+      this.render(canvas, ctx, gravityX, gravityY)
+      requestId=canvas.requestAnimationFrame(renderLoop)
     }
-    canvas.requestAnimationFrame(renderLoop)
+   //监听手机速度计
+    wx.onAccelerometerChange(({
+      x,
+      y,
+      z
+    }) => {
+      canvas.cancelAnimationFrame(requestId)
+      renderLoop(0, y)
+     
+    })
   },
-  render(canvas, ctx) {
+  //渲染每个球
+  render(canvas, ctx, gravityX, gravityY) {
     ctx.clearRect(0, 0, this.systemWidth, this.systemHeight)
     this.data.lists.forEach((item, index) => {
-      this.drawBall(ctx, item, index)
+      this.drawBall(ctx, item, index, gravityX, gravityY)
     })
-
   },
   //绘制球球
-  drawBall(ctx, config, index) {
+  drawBall(ctx, config, index, gravityX, gravityY) {
     const {
       radius,
       color,
       font,
-      position,
-      count,
-      translateX,
+      defaultPosition: position,
       fontsize
     } = config;
     const p = position
     p.y += p.vy
-    //第一次下落  触底了  
-    if (p.y >= this.systemHeight - radius && count === 1) {
-      p.vy = -10
-      this.data.lists[index].count = 2
-      this.setData({
-        lists: this.data.lists
-      })
-      console.log(this.data.lists)
-    }
-    //第一次回弹  距离顶部高度达到500  
-    if (p.y <= this.systemHeight - 200 && count === 2) {
-      p.vy = 8
-      this.data.lists[index].count = 3
-      this.setData({
-        lists: this.data.lists
-      })
-
-    }
-    // //第二次下落  
-    if (p.y >= this.systemHeight - radius && count === 3) {
-      p.vy = -5
-      this.data.lists[index].count = 4
-      this.setData({
-        lists: this.data.lists
-      })
-    }
-    //第二次回弹
-    if (p.y < this.systemHeight - 80 && count === 4) {
-      p.vy = 3
-      this.data.lists[index].count = 5
-      this.setData({
-        lists: this.data.lists
-      })
-    }
-    // //第三次下落  
-    if (p.y >= this.systemHeight - radius && count === 5) {
-      p.vy = 0
-      p.x += p.vx
-      if (p.x >= translateX) {
-        p.vx = 0
-      }
-    }
+    p.x += p.vx
+    this.getTrack(p, radius, index, gravityX, gravityY)
     ctx.beginPath()
     ctx.arc(p.x, p.y, radius, 0, Math.PI * 2)
     ctx.fillStyle = color
     ctx.fill()
-    ctx.strokeText(font, p.x - 15, p.y + 13)
+    ctx.strokeText(font, p.x - radius/2, p.y + radius/2-5)
     ctx.strokeStyle = 'yellow'
-    ctx.font = fontsize
+    ctx.font = '20px sans-serif'
   },
 
-  onShow: function () {
+  //判断下落轨迹 
+  getTrack(p, radius, index, gravityX, gravityY) {
+    
+    if( 0 <= gravityY && gravityY < 1 ){
+      if ( p.y <= radius ) {
+        p.vy = 2
+       
+      }else{
+        p.vy = -2
+      }
+      this.data.lists[index].defaultPosition.y = p.y
+      this.data.lists[index].defaultPosition.vy = p.vy
+      this.setData({
+        lists: this.data.lists
+      })
+      
+    }
+    if(-1 <= gravityY && gravityY < 0){
+      if (p.y >= this.systemHeight - radius) {
+        p.vy = -1
+      }else{
+        p.vy = 1
+      }
+      this.data.lists[index].defaultPosition.y = p.y
+      this.data.lists[index].defaultPosition.vy = p.vy
+      this.setData({
+        lists: this.data.lists
+      })
+    }
+
+   
+    
+    // if(-1 <= gravityX && gravityX <= 0){
+    //   // console.log(gravityX)
+
+    //   if (p.x >= this.systemWidth - radius) {
+    //     console.log(1111)
+    //     p.vx = -2
+    //   }else{
+    //     // console.log(22222)
+    //     p.vx = 2
+    //   }
+    //   this.data.lists[index].defaultPosition.x = p.x
+    //   this.data.lists[index].defaultPosition.vx = p.vx
+    //   this.setData({
+    //     lists: this.data.lists
+    //   })
+    // }
+   
+    if (p.x >= this.systemWidth - radius || p.x <= radius) {
+      p.vx = -p.vx
+      this.data.lists[index].defaultPosition.x = p.x
+      this.setData({
+        lists: this.data.lists
+      })
+    }
+
   
+
+  },
+
+
+  //判断两个球球是否碰撞  两个圆心的距离是否大于 两个圆半径之和
+  getCrash(circle1, circle2) {
+    let {
+      x1,
+      y1,
+      r1
+    } = circle1;
+    let {
+      x2,
+      y2,
+      r2
+    } = circle2;
+    let distance = Math.sqrt(Math.pow(x1 - x2) + Math.pow(y1 - y2))
+    if (distance <= r1 + r2) {
+      //碰撞了
+      this.setData({
+        isCrash: true
+      })
+    }
+    if (distance > r1 + r2) {
+      //没有碰撞
+      this.setData({
+        isCrash: false
+      })
+    }
+
+  },
+
+  // 实现小圆自由落体运动
+  fall(height, a = 98) {
+    let time = Math.sqrt(2 * height / a)
+    this.animate(time, y => {
+      this.drawCircle(this.x, this.radius + y, this.radius)
+    }, passedTime => 1 / 2 * a * Math.pow(passedTime, 2))
+  },
+
+
+
+  onShow: function () {
+
   }
 
 
